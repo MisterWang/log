e50c839621a2189f82dcc7a17730f0e91b86c0c6
liu <827947510@qq.com>
2017-10-13 16:52:45 +0800

    Create configure.sh

---
+++ php/configure.sh
@@ -0,0 +1,36 @@
+./configure \
+--prefix=/usr/local/php-$phpversion \
+--with-config-file-path=/usr/local/php-$phpversion/etc \
+--enable-redis \
+--with-mysql \
+--with-mysqli \
+--with-pdo-mysql \
+--enable-fpm \
+--with-curl \
+--enable-memcache \
+--enable-mongo \
+--with-pcre-regex \
+--with-zlib \
+--with-gd \
+--with-png-dir \
+--with-jpeg-dir \
+--with-vpx-dir \
+--enable-gd-native-ttf \
+--enable-gd-jis-conv \
+--with-freetype-dir \
+--with-bz2 \
+--with-openssl \
+--enable-bcmath \
+--with-kerberos \
+--enable-ftp \
+--with-gettext \
+--enable-opcache \
+--enable-pcntl \
+--enable-zip \
+--with-pear \
+--with-imagick \
+--enable-mbstring \
+--enable-exif
+
+#--with-xpm-dir \
+#--with-mcrypt \
