7e74a13624f71e1bfe63a80d842e445f4b031e0f
liu <827947510@qq.com>
2018-01-25 10:06:07 +0800

    Create ActivityRecord的迭代器.php

---
+++ 编程语言/php/yii/ActivityRecord的迭代器.php
@@ -0,0 +1,19 @@
+实现接口
+```php
+class Model extends Component implements StaticInstanceInterface, IteratorAggregate, ArrayAccess, Arrayable
+
+```
+
+实现IteratorAggregate接口
+```php
+/**
+ * Returns an iterator for traversing the attributes in the model.
+ * This method is required by the interface [[\IteratorAggregate]].
+ * @return ArrayIterator an iterator for traversing the items in the list.
+ */
+public function getIterator()
+{
+    $attributes = $this->getAttributes();
+    return new ArrayIterator($attributes);
+}
+```
